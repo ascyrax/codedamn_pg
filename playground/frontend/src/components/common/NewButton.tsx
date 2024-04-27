26db24e04e19462d6b3029f77984b1c1830f8db6
ascyrax <ascyrax01@gmail.com>
2024-04-27 10:41:51 +0530

    added dist folder for netlify

--- playground/frontend/src/components/common/NewButton.tsx
+++ playground/frontend/src/components/common/NewButton.tsx
@@ -3,12 +3,12 @@ export default function NewButton() {
     <>
       <span className="span-newButton">
         <img
-          src="../../public/newFile.png"
+          src="public/newFile.png"
           className="newButton"
           id="newFileButton"
         ></img>
         <img
-          src="../../public/new-folder.png"
+          src="public/newFolder.png"
           className="newButton"
           id="newFolderButton"
         ></img>
