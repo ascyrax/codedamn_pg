cc7d9c5034e6a81155b63ab8a2cf690fc1449bab
ascyrax <ascyrax01@gmail.com>
2024-04-29 07:25:06 +0530

    dockerode progress. todo: websockets for xterm-dockerode interactive shells

--- playground/backend/src/api/controllers/terminalController.js
+++ playground/backend/src/api/controllers/terminalController.js
@@ -5,16 +5,6 @@ import * as Docker from "dockerode";
 // dockerode
 const docker = new Docker.default({ socketPath: "/var/run/docker.sock" });
 
-// nodepty
-// var shell = os.platform() === "win32" ? "powershell.exe" : "zsh";
-
-// var ptyProcess = pty.spawn(shell, [], {
-//   name: "codedamn",
-//   cols: 100,
-//   rows: 40,
-//   // cwd: "./",
-//   env: process.env,
-// });
 // docker.listContainers({ all: true }, function (err, containers) {
 //   if (err) {
 //     console.log("Error listing containers:", err);
@@ -31,62 +21,32 @@ const docker = new Docker.default({ socketPath: "/var/run/docker.sock" });
 //   }
 // });
 
-// Container options
-const containerOptions = {
-  Image: "ubuntu", // Specify the image name
-  // Cmd: ["bash", "-c", 'while true; do echo "Hello, Dockerode!"; sleep 1; done'],
-  Cmd: ["bash", "-c", "tail -f /dev/null"],
-  name: "user01",
-  AttachStdout: true, // Attach container's stdout to the Node.js process
-  AttachStderr: true, // Attach container's stderr to the Node.js process
-  Tty: true,
-};
-
-// Create and start the container
-docker.createContainer(containerOptions, function (err, container) {
-  if (err) {
-    console.error("Error creating container:", err);
-    return;
-  }
-
-  container.start(function (err) {
+function createAndStartContainer(containerOptions) {
+  // Create and start the container
+  docker.createContainer(containerOptions, function (err, container) {
     if (err) {
-      console.error("Error starting container:", err);
+      console.error("Error creating container:", err);
       return;
     }
 
-    console.log("Container started successfully!");
-
-    // Listen for container events
-    container.wait(function (err, data) {
+    container.start(function (err) {
       if (err) {
-        console.error("Error waiting for container:", err);
+        console.error("Error starting container:", err);
         return;
       }
 
-      console.log("Container stopped:", data);
+      console.log("Container started successfully!");
+
+      // Listen for container events
+      container.wait(function (err, data) {
+        if (err) {
+          console.error("Error waiting for container:", err);
+          return;
+        }
+
+        console.log("Container stopped:", data);
+      });
     });
-  });
-});
-
-export function executeCommandInContainer(req, res) {
-  let command = req.body.body || "ls -la";
-  // let command = "ls -la";
-
-  console.log(command);
-
-  // execute in container
-  let userContainerId = "user01"; // container corresponding to the user
-  let container = docker.getContainer(userContainerId);
-
-  executeCommand(container, command, (err, output) => {
-    if (err) {
-      console.error("Error:", err);
-      res.status(500).send({ error: "Failed to execute command" });
-    } 
-    else {
-      res.send({ output: output });
-    }
   });
 }
 
@@ -96,6 +56,7 @@ function executeCommand(container, command, callback) {
       Cmd: ["bash", "-c", command],
       AttachStdout: true,
       AttachStderr: true,
+      Tty: true,
     },
     function (err, exec) {
       if (err) return callback(err);
@@ -122,15 +83,122 @@ function executeCommand(container, command, callback) {
   );
 }
 
-// function runCommandInNodepty(command) {
-//   let ptyResponse = "";
-//   ptyProcess.onData((data) => {
-//     ptyResponse += data;
-//   });
+async function isContainerRunning(container, userContainerId) {
+  // check if the container is running or not
+  try {
+    const data = await container.inspect();
+    // console.log(data);
 
-//   ptyProcess.write(command);
-//   setTimeout(() => {
-//     console.log(ptyResponse);
-//     res.send(ptyResponse);
-//   }, 1000);
-// }
+    // Check the state of the container
+    if (data.State.Running) {
+      console.log(`The container '${userContainerId}' is running.`);
+      return true;
+    } else {
+      console.log(`The container '${userContainerId}' is not running.`);
+      return false;
+    }
+  } catch (error) {
+    console.error(
+      `:( terminalController.js / isContainerRunning(): error trying to inspect the container ${userContainerId}`,
+      error.message
+    );
+    return false;
+  }
+}
+
+async function createInteractiveShell(container, containerName) {
+  // const container = await docker.createContainer({
+  //   Image: "ubuntu",
+  //   Cmd: ["/bin/bash"],
+  //   Tty: true,
+  //   name: containerName,
+  //   AttachStdout: true,
+  //   AttachStderr: true,
+  //   OpenStdin: true,
+  // });
+
+  // await container.start();
+
+  const exec = await container.exec({
+    AttachStdin: true,
+    AttachStdout: true,
+    AttachStderr: true,
+    Tty: true,
+    Cmd: ["/bin/bash"],
+  });
+
+  const execStream = await exec.start({ hijack: true, stdin: true });
+  process.stdin.pipe(execStream);
+  execStream.pipe(process.stdout);
+
+  execStream.on("end", () => {
+    console.log("Stream ended");
+    process.exit();
+  });
+
+  process.stdin.on("end", () => {
+    console.log("STDIN ended");
+    execStream.end();
+  });
+
+  process.stdin.setRawMode(true);
+}
+
+export async function executeCommandInContainer(req, res) {
+  let command = req.body.body || "ls -la";
+  // let command = "ls -la";
+
+  console.log(command);
+
+  // execute in container
+  let userContainerId = "user01"; // container corresponding to the user
+
+  let container;
+  try {
+    container = await docker.getContainer(userContainerId);
+  } catch (err) {
+    console.log(
+      ":( could not get the coontainer using docker.getContainer: ",
+      err
+    );
+    res.status(500).send({ error: "Failed to execute command" });
+  }
+
+  // create the container if it doesn't exits
+  // Container options
+  const containerOptions = {
+    Image: "ubuntu", // Specify the image name
+    // Cmd: ["bash", "-c", 'while true; do echo "Hello, Dockerode!"; sleep 1; done'],
+    Cmd: ["bash", "-c", "tail -f /dev/null"],
+    name: userContainerId,
+    AttachStdout: true, // Attach container's stdout to the Node.js process
+    AttachStderr: true, // Attach container's stderr to the Node.js process
+    Tty: true,
+  };
+  // createAndStartContainer(containerOptions);
+
+  let isRunning = await isContainerRunning(container, userContainerId);
+
+  // start the container if paused/stopped
+  if (!isRunning) {
+    try {
+      await container.start();
+      console.log(`The container '${userContainerId}' has been started.`);
+    } catch (err) {
+      console.log(
+        `:( could not start the paused/stopped container ${userContainerId}`
+      );
+    }
+  }
+
+  createInteractiveShell(container, userContainerId);
+
+  // executeCommand(container, command, (err, output) => {
+  //   if (err) {
+  //     console.error("Error:", err);
+  //     res.status(500).send({ error: "Failed to execute command" });
+  //   } else {
+  //     res.send({ output: output });
+  //   }
+  // });
+}
--- playground/bugs.md
+++ playground/bugs.md
@@ -1,2 +1,9 @@
 batchUpdate was running on every call from setState.
 reason: it was being recreated after each react element re-render, since it was present inside the react component.
+
+
+missing await on an async function, causing everything to work agains the logic. i tried using logs, but everything was doing weird things.
+
+
+could not do some things using http, since its stateless, so used websockets instead.
+things like, xterm to dockerode interactive shell session
