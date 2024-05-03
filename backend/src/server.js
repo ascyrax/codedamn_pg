e7a19199b27d0866301e1265b6fc53531d07a8c4
ascyrax <ascyrax01@gmail.com>
2024-05-03 07:03:26 +0530

    fixed. i was missing http in the url

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,48 +11,11 @@ import { authenticateToken } from "./middlewares/auth.js";
 import { handleNewWSConnection } from "./realtime/wsHandlers.js";
 
 const app = express();
-// CORS configuration
-// const corsOptions = {
-//   origin: "*", // Your EC2 instance's public DNS
-//   // credentials: true, // If you need credentials such as cookies, authorization headers or TLS client certificates
-// };
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
-
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
-// app.use(cors(corsOptions));
 
 dotenv.config();
 
 app.use(express.json());
 
-// app.use(cors());
-// app.use(
-//   cors({
-//     origin: [
-//       "http://13.201.4.165/",
-//       "http://13.201.4.165:80",
-//       "http://ec2-13-201-4-165.ap-south-1.compute.amazonaws.com/",
-//       "http://ec2-13-201-4-165.ap-south-1.compute.amazonaws.com:80",
-//       "http://localhost:80",
-//       "http://localhost",
-//       "*"
-//     ],
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
     origin: (origin, callback) => {
